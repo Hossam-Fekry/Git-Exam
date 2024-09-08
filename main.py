e1dd49ea58c18c0ad1d98e4f3215d02c62571e58
Hossam-Fekry <hossamfekryed@gmail.com>
2024-09-08 21:46:24 +0300

    complete the buttom

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from tkinter import messagebox
 root = Tk()
 root.resizable(0,0)
 root.configure(bg="#000000")
-
 root.title("awesome button")
 root.geometry("300x300")
-b = Button(root,text="click me",bg = "#FFFFFF").pack()
+
+def the_func():
+    m = messagebox.askokcancel("EXIT","Do you want to exit?")
+    if m == True:
+        exit()
+
+b = Button(root,text="Exit",bg = "#FFFFFF",command=the_func).pack()
 root.mainloop()
