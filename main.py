a55195213350f88b790c82d20a5de73ccfb188bb
Hossam-Fekry <hossamfekryed@gmail.com>
2024-09-08 21:23:55 +0300

    fix the spelling

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ root = Tk()
 root.resizable(0,0)
 root.configure(bg="#000000")
 
-root.title("awesome buttom")
+root.title("awesome button")
 root.geometry("300x300")
 b = Button(root,text="click me",bg = "#FFFFFF").pack()
 root.mainloop()
